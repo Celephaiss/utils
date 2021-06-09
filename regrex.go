378931109bf07fbf288488e14be06e7b1d845e22
ailu <ailu@sogou-inc.com>
2021-06-09 15:37:53 +0800

    update

---
+++ regrex.go
@@ -0,0 +1,9 @@
+package utils
+
+//var baikeRegex = regexp.MustCompile(`d(\d+)\.htm`)
+
+// if l := baikeRegex.FindStringIndex(title); l != nil {
+//			title = title[:l[0]]
+//		} else {
+//			return true
+//		}
